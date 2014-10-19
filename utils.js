f880c529c40d8f16fb6000539afbdb7c56bf342c
George Du <gdu@mit.edu>
2014-10-19 01:22:29 -0400

    added utils

---
+++ utils.js
@@ -0,0 +1,17 @@
+var loggedIn = function(request, response, next) {
+  if (request.user) {
+    next();
+  }
+  else {
+    response.status(401).send('Not Authorized');
+  }
+}
+
+var handleError = function(err) {
+  throw err;
+}
+
+module.exports = {
+  loggedIn: loggedIn,
+  handleError: handleError
+}
