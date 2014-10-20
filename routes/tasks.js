94384e2628f97881fc80d9e89705c925a8f3eee9
Rujia Zha <rujiazha@gmail.com>
2014-10-19 20:52:59 -0400

    modified tasks.js to incorporate utils.js, Rujia

--- routes/tasks.js
+++ routes/tasks.js
@@ -11,7 +11,7 @@ router.get('/:id/tasks', utils.loggedIn, function(request, response) {
 	var userTasks = [];
 	Projects.findOne({ _id: request.params.id }, function(projectErr, project) {
 		utils.handleError(projectErr);
-		else if (!project) {
+		if (!project) {
 			response.send('Project ' + request.params.id + ' cannot be found');
 			return;
 		}
@@ -20,7 +20,7 @@ router.get('/:id/tasks', utils.loggedIn, function(request, response) {
 		for (var i = 0; i < taskIdList.length; i++) {
 			Tasks.findOne({ _id: taskIdList[i] }, function(taskErr, task) {
 				utils.handleError(taskErr);
-				else if (!task) {
+				if (!task) {
 					console.log('Task with id '+ taskIdList[i] + ', which is part of project ' + request.params.id+ ', cannot be found');
 				}
 				else if (task.assignee === request.user.username) {
@@ -47,9 +47,7 @@ router.post('/:id/tasks', utils.loggedIn, function(request, response) {
  			utils.handleError(err);
  			Users.update({ username: request.user.username}, { $push: { tasks: newTask._id }}, function(err) {
 	 			utils.handleError(err);
-	 			else {
-	 				response.json({success: true});
-	 			}
+	 			response.json({success: true});
  			});
  		});	
 	});
@@ -60,7 +58,7 @@ router.get('/:id1/tasks/:id2', function(request, response) {
   //console.log(request.params);
 	Tasks.findOne( {_id: request.params.id2 }, function(taskErr, foundTask) {
 		utils.handleError(taskErr);
-		else if (!foundTask) {
+		if (!foundTask) {
 			response.json({ success: false });
 		}
 		else {
@@ -78,7 +76,7 @@ router.post('/:id1/tasks/:id2', function(request, response) {
 		completion: request.body.completion,
 		deadline: newDeadline}, function(err, numAffected) {
 			utils.handleError(err);
-			else if (numAffected === 0) {
+			if (numAffected === 0) {
 				response.json({ success: false});
 			}
 			else {
@@ -91,17 +89,13 @@ router.post('/:id1/tasks/:id2', function(request, response) {
 router.delete('/:id1/tasks/:id2', utils.loggedIn, function(request, response) {
 	Tasks.remove({ _id: request.params.id2 }, function(err) {
 		utils.handleError(err);
-		else {
-			Projects.update({ _id: request.params.id1 }, { $pull: { tasks: request.params.id2 }}, function(err) {
+		Projects.update({ _id: request.params.id1 }, { $pull: { tasks: request.params.id2 }}, function(err) {
+ 			utils.handleError(err);
+ 			Users.update({ username: request.user.username}, { $pull: { tasks: request.params.id2 }}, function(err) {
 	 			utils.handleError(err);
-	 			Users.update({ username: request.user.username}, { $pull: { tasks: request.params.id2 }}, function(err) {
-		 			utils.handleError(err);
-		 			else {
-		 				response.json({success: true});
-		 			}
-	 			});
-	 		});	
-		}
+	 			response.json({success: true});
+ 			});
+ 		});	
 	});
 });
 
