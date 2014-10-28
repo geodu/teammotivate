7c02b5cfd4f2c72b5c13b896322818fa410c52ef
dchoi2 <dchoi2@mit.edu>
2014-10-28 00:04:55 -0400

    in progress

--- public/app/home/home.js
+++ public/app/home/home.js
@@ -17,11 +17,17 @@ angular.module('teamMotivate')
 	'$scope',
 	'projects',
 	'session',
-	function($scope, projects, session) {
+  'tasks',
+	function($scope, projects, session, tasks) {
   	console.log(projects);
   	console.log(session);
   	$scope.user = session.name;
   	$scope.projects = projects.projects;
+    $scope.tasks = {};
+    for (project in $scope.projects) {
+      $scope.tasks[project._id] = tasks.get(project._id);
+    }
+
 
     $scope.isLoggedIn = function() {
       return session.name !== undefined;
